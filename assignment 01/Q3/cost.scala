399cfe1fc95b36267bad8ca4fa238fdc4ae6aab2
randiepathirage <52100121+randiepathirage@users.noreply.github.com>
2020-05-24 16:37:52 +0530

    update changes

--- assignment 01/Q3/cost.scala
+++ assignment 01/Q3/cost.scala
@@ -1,9 +1,8 @@
 object cost extends App{
-	def shipping(x:Int)=if(x<50)3*50 else 3*50+(x-50)*.75;
-	def coverPrice(x:Int)=24.95*x;
-	def discount(x:Int)=x*.4;
+	def shipping(x:Int)=if(x<50) 50*3 else 3*50+(x-50)*.75;
+	def Price(x:Int)=24.95*x*0.6;
 
 
-	def cost(x:Int)=coverPrice(x)-discount(x)+shipping(x);
+	def cost(x:Int)=Price(x)+shipping(x);
 	println(cost(60));
 }
