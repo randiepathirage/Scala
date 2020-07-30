128cd5bd57f408e1a54ac482d7e775c43516f229
randiepathirage <52100121+randiepathirage@users.noreply.github.com>
2020-07-30 11:18:26 +0530

    q1

--- assignment 05/question 01/negative.scala
+++ assignment 05/question 01/negative.scala
@@ -2,23 +2,30 @@ import io.StdIn._
 
 object Negative extends App{
 
-	//take input from user
-	print("ENTER NUMERATOR:")
-	var num=readInt()
+	val x = new Rational(3,4)
+	var negativeX=x.neg
+	println(negativeX)
 
-	print("ENTER DENOMINATOR:")
-	var den=readInt()
+	val z = new Rational(2)
+	var negativeZ=z.neg
+	println(negativeZ)
 
-	val r1=new Rational(num,den)
-	var negative=r1.neg
-	println(negative)
+   	val y = new Rational(5,0)
+	var negativeY=y.neg
+	println(negativeY)
 }
 
 class Rational(n:Int,d:Int){
+	require(d>0,"d must be greater than 0")	
+
+	def numer=n/GCD(n,d)
+        def denom=d/GCD(n,d)
+
+	//constructor
+	def this(n:Int)=this(n,1)
 
 	private def GCD(a:Int,b:Int):Int= { if (b==0 ) a else GCD(b,a%b) }
-        def numer=n/GCD(n,d)
-        def denom=d/GCD(n,d)
+        
 
 	def neg= new Rational(-this.numer,this.denom)
 
